a330f205f36286d2a9ed19359e7765617f298aa8
Ojas Shirekar <ojas.shirekar@pm.me>
2021-11-12 09:13:11 +0100

    updated the job to remove references to redis

--- tune_job.sh
+++ tune_job.sh
@@ -5,7 +5,6 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --nodes=5
-#SBATCH --exclusive
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
 
@@ -22,47 +21,61 @@ module load miniconda/3.9
 
 source activate /home/nfs/oshirekar/unsupervised_ml/ai2
 
-# ===== DO NOT CHANGE THINGS HERE UNLESS YOU KNOW WHAT YOU ARE DOING =====
-# This script is a modification to the implementation suggest by gregSchwartz18 here:
-# https://github.com/ray-project/ray/issues/826#issuecomment-522116599
-redis_password=$(uuidgen)
-export redis_password
+set -x
 
-nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST") # Getting the node names
+# __doc_head_address_start__
+
+# Getting the node names
+nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
 nodes_array=($nodes)
 
-node_1=${nodes_array[0]}
-ip=$(srun --nodes=1 --ntasks=1 -w "$node_1" hostname --ip-address) # making redis-address
+head_node=${nodes_array[0]}
+head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 
 # if we detect a space character in the head node IP, we'll
 # convert it to an ipv4 address. This step is optional.
-if [[ "$ip" == *" "* ]]; then
-  IFS=' ' read -ra ADDR <<< "$ip"
-  if [[ ${#ADDR[0]} -gt 16 ]]; then
-    ip=${ADDR[1]}
-  else
-    ip=${ADDR[0]}
-  fi
-  echo "IPV6 address detected. We split the IPV4 address as $ip"
+if [[ "$head_node_ip" == *" "* ]]; then
+IFS=' ' read -ra ADDR <<<"$head_node_ip"
+if [[ ${#ADDR[0]} -gt 16 ]]; then
+  head_node_ip=${ADDR[1]}
+else
+  head_node_ip=${ADDR[0]}
 fi
+echo "IPV6 address detected. We split the IPV4 address as $head_node_ip"
+fi
+# __doc_head_address_end__
 
+# __doc_head_ray_start__
 port=6379
-ip_head=$ip:$port
+ip_head=$head_node_ip:$port
 export ip_head
 echo "IP Head: $ip_head"
 
-echo "STARTING HEAD at $node_1"
-srun --nodes=1 --ntasks=1 -w "$node_1" \
-  ray start --head --node-ip-address="$ip" --port=$port --redis-password="$redis_password" --block &
-sleep 30
+echo "Starting HEAD at $head_node"
+srun --nodes=1 --ntasks=1 -w "$head_node" \
+    ray start --head --node-ip-address="$head_node_ip" --port=$port \
+    --num-cpus "${SLURM_CPUS_PER_TASK}" --num-gpus "${SLURM_GPUS_PER_TASK}" --block &
+# __doc_head_ray_end__
 
-worker_num=$((SLURM_JOB_NUM_NODES - 1)) #number of nodes other than the head node
+# __doc_worker_ray_start__
+# optional, though may be useful in certain versions of Ray < 1.0.
+sleep 10
+
+# number of nodes other than the head node
+worker_num=$((SLURM_JOB_NUM_NODES - 1))
+
 for ((i = 1; i <= worker_num; i++)); do
-  node_i=${nodes_array[$i]}
-  echo "STARTING WORKER $i at $node_i"
-  srun --nodes=1 --ntasks=1 -w "$node_i" ray start --address "$ip_head" --redis-password="$redis_password" --block &
-  sleep 5
+    node_i=${nodes_array[$i]}
+    echo "Starting WORKER $i at $node_i"
+    srun --nodes=1 --ntasks=1 -w "$node_i" \
+        ray start --address "$ip_head" \
+        --num-cpus "${SLURM_CPUS_PER_TASK}" --num-gpus "${SLURM_GPUS_PER_TASK}" --block &
+    sleep 5
 done
+# __doc_worker_ray_end__
+
+# __doc_script_start__
+# ray/doc/source/cluster/examples/simple-trainer.py
 
 # ===== Call your code below =====
 python -u tuner.py "$SLURM_CPUS_PER_TASK"
