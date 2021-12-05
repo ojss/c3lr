81c9c9b5433937f397862a25527d6858391ac53b
Ojas Shirekar <ojas.shirekar@gmail.com>
2021-12-05 14:56:11 +0100

    modifying hydra launcher

--- hlauncher.py
+++ hlauncher.py
@@ -5,15 +5,21 @@ from hydra_zen import launch
     ExperimentConfig,
     task_function,
     overrides=[
-        "hydra/launcher=submitit_local",
-        "params.batch_size=50, 100, 200",
-        "trainer.gpus=0",
+        "hydra/launcher=submitit_slurm",
+        "params.rdim_components=2",
+        "trainer.gpus=1",
         "trainer.fast_dev_run=True",
         "hydra/job_logging=colorlog", 
         "hydra/hydra_logging=colorlog",
-        "hydra/sweeper=optuna",
-        "hydra.sweeper.direction=maximize",
-        "hydra.sweeper.n_trials=3"
+        # "hydra/sweeper=optuna",
+        # "hydra.sweeper.direction=maximize",
+        # "hydra.sweeper.n_trials=1"
+        "hydra.launcher.mem_gb=24",
+        "hydra.launcher.tasks_per_node=1",
+        "hydra.launcher.nodes=4",
+        "hydra.launcher.partition=general",
+        "hydra.launcher.timeout_min=30",
+        "hydra.launcher.additional_parameters.qos=short" 
     ],
     multirun=True,
 )
